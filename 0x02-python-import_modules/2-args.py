c035b3e1b655972c935b938eb351c7e1cc30c5fa
Hassan Juma <okothhassanjuma@gmail.com>
2021-09-09 22:23:17 +0300

    add arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    count = len(sys.argv)
+    if count == 1:
+        print("0 arguments.")
+    elif count == 2:
+        print("1 argument:")
+    else:
+        print("{} arguments:".format(count - 1))
+    for i in range(1, count):
+        print("{}: {}".format(i, sys.argv[i]))
