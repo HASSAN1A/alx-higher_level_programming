c0d5da0d94a825caa6b0d134655a3360ae60ea0f
Hassan Juma <okothhassanjuma@gmail.com>
2021-09-28 23:22:39 +0300

    Add square  2

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""define a class Square"""
+
+
+class Square:
+    """private size attribute for Square class"""
+    def __init__(self, size=0):
+        if not isinstance(size, int):
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
