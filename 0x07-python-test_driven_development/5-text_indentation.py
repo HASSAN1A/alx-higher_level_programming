5ae532fc74c66839f87361eacca003e6482563d3
Hassan Juma <okothhassanjuma@gmail.com>
2021-09-30 22:00:36 +0300

    Add text indentation

---
+++ 0x07-python-test_driven_development/5-text_indentation.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Module that defines a function that prints an indented text"""
+
+
+def text_indentation(text):
+    """Prints a text with 2 new lines after '.', '?', and ':'.
+    Args:
+        text (string)
+    Raises:
+        TypeError: If text is not a string.
+    """
+    if not isinstance(text, str):
+        raise TypeError("text must be a string")
+
+    c = 0
+    while c < len(text) and text[c] == ' ':
+        c += 1
+
+    while c < len(text):
+        print(text[c], end="")
+        if text[c] == "\n" or text[c] in ".?:":
+            if text[c] in ".?:":
+                print("\n")
+            c += 1
+            while c < len(text) and text[c] == ' ':
+                c += 1
+            continue
+        c += 1
