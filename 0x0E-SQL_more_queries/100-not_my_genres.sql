be6e37d278a5b540df5f5e473ef8f934d6f6d0ce
HASSAN1A <okothhassanjuma@gmail.com>
2021-11-18 01:10:25 +0300

    inner joins on dexter

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- it uses the hbtn_0d_tvshows database to list all genres not linked to the show Dexter
+SELECT DISTINCT a.name FROM tv_genres AS a
+       INNER JOIN tv_show_genres AS b ON a.id = b.genre_id
+       INNER JOIN tv_shows AS c ON b.show_id = c.id
+WHERE
+	a.name NOT IN
+        (SELECT a.name FROM tv_genres AS a
+		INNER JOIN tv_show_genres AS b ON a.id = b.genre_id
+		INNER JOIN tv_shows AS c ON b.show_id = c.id
+	 WHERE c.title = "Dexter")
+ ORDER BY a.name;
