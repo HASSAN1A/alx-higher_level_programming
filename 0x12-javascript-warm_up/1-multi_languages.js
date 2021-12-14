8cbcc7e37cb06b42d7f452f147ede8615e20b26d
HASSAN1A <okothhassanjuma@gmail.com>
2021-12-14 22:41:09 +0300

    Add multilanguage

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+const cMessage = 'C is fun';
+const pythonMessage = 'Python is cool';
+const jsMessage = 'JavaScript is amazing';
+console.log(cMessage);
+console.log(pythonMessage);
+console.log(jsMessage);
